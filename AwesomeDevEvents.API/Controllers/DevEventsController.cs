78519ac071a7936379ae00f3692afe30b0bee532
fonseca.william <fonseca.william@greenbeltti.onmicrosoft.com>
2023-07-17 00:26:18 -0300

    feat: estudos da aula 1

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,104 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+
+        private readonly DevEventsDbContext _context;
+
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // api/dev-events GET
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(d => !d.IsDeleted).ToList();
+            return Ok(devEvents);
+        }
+
+        // api/dev-events/12312312 GET
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if(devEvent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(devEvent);
+
+        }
+
+        // api/dev-events POST
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+            _context.DevEvents.Add(devEvent);
+
+            return CreatedAtAction(nameof(GetById), new {id = devEvent.Id} , devEvent);
+
+        }
+
+        // api/dev-events/12345321 PUT
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id,DevEvent devEventInput)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(devEventInput.Title, devEventInput.Description, devEventInput.StartDate, devEventInput.EndDate);
+
+            return NoContent();
+
+        }
+
+        // api/dev-events/123123 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            return NoContent();
+        }
+
+        // api/dev-events/3fa85f64-5717-4562-b3fc-2c963f66afa6/speakers POST
+        [HttpPost("{id}/speakers")]
+        public IActionResult PostSpeaker(Guid id, DevEventSpeaker speaker)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Speakers.Add(speaker);
+
+            return NoContent();
+
+        }
+
+    }
+}
