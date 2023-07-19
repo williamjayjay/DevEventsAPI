37ccd541759c2f4d9413873055d1cd2319cb726a
fonseca.william <fonseca.william@greenbeltti.onmicrosoft.com>
2023-07-19 04:41:06 -0300

    feat: add pacotes e cria migration

--- AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents.API/Program.cs
@@ -6,7 +6,7 @@ System.Net.ServicePointManager.ServerCertificateValidationCallback = (sender, ce
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DEV_DB");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<DevEventsDbContext>(o => o.UseSqlServer(connectionString));
 
